dbc65f4cb58355b11acac21ec6d0c20821262385
ZHANG <ZHANGKUN12138000@users.noreply.github.com>
2026-09-10 00:33:19 +0800

    V0.2

--- abaqus_plugins/atd/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""ATD Abaqus-to-LS-DYNA conversion core."""
-
-from .converter import ConversionError, convert_file
-
-__all__ = ['ConversionError', 'convert_file']
-__version__ = '1.0.2'
